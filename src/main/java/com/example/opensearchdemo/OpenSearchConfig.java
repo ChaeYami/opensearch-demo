4ba90120b87abe16c714a7efba993d3f8bd6aab6
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2024-08-08 20:46:47 +0900

    Update OpenSearchConfig.java

--- src/main/java/com/example/opensearchdemo/OpenSearchConfig.java
+++ src/main/java/com/example/opensearchdemo/OpenSearchConfig.java
@@ -24,25 +24,60 @@ import java.io.File;
 @Configuration
 public class OpenSearchConfig {
 
+//    private static final String SCHEME = "https";
+    private static final String SCHEME = "http";
+//    private static final String HOST = "localhost";
+    private static final String HOST = "211.254.212.171";
+    private static final int PORT = 9200;
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "Pass123Word456!!";
+
+    private static final String TRUSTSTOREDIR = "C:/Users/SCY/mytruststore.jks";
+    private static final String TRUSTSTOREPW = "mypassword";
+
+
+    /**
+     * OpenSearchClient Bean 설정
+     *
+     * @return OpenSearchClient
+     */
+    @Bean
+    public OpenSearchClient openSearchClient() {
+
+        final HttpHost httpHost = new HttpHost(SCHEME, HOST, PORT);
+
+        // 인증 정보를 설정
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(new AuthScope(httpHost),
+                new UsernamePasswordCredentials(USERNAME, PASSWORD.toCharArray()));
+
+        // OpenSearch와 통신하기 위한 OpenSearchTransport 객체를 생성
+        final OpenSearchTransport transport =
+                ApacheHttpClient5TransportBuilder.builder(httpHost)
+                        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                                .setDefaultCredentialsProvider(credentialsProvider)).build();
+
+        return new OpenSearchClient(transport);
+    }
+
+    /*
     @Bean
     public OpenSearchClient openSearchClient() throws Exception {
         // TrustStore 설정
-        System.setProperty("javax.net.ssl.trustStore", "C:/Users/SCY/Desktop/opensearch-demo/mytruststore.jks");
-        System.setProperty("javax.net.ssl.trustStorePassword", "mypassword");
+        System.setProperty("javax.net.ssl.trustStore", TRUSTSTOREDIR);
+        System.setProperty("javax.net.ssl.trustStorePassword", TRUSTSTOREPW);
 
-        final HttpHost host = new HttpHost("https", "211.253.36.208", 9200);
+        final HttpHost host = new HttpHost(SCHEME, HOST, PORT);
         final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(new AuthScope(host), new UsernamePasswordCredentials("admin", "Pass123Word456!!".toCharArray()));
+        credentialsProvider.setCredentials(new AuthScope(host), new UsernamePasswordCredentials(USERNAME, PASSWORD.toCharArray()));
 
         final SSLContext sslContext = SSLContextBuilder.create()
                 .loadTrustMaterial(null, (chains, authType) -> true) // 모든 인증서 신뢰
                 .build();
 
-        /*final SSLContext sslContext = SSLContextBuilder.create()
-                .loadTrustMaterial(new File("C:/Users/SCY/mytruststore.jks"), "mypassword".toCharArray())
-                .build();*/
-
-
+//        final SSLContext sslContext = SSLContextBuilder.create()
+//                .loadTrustMaterial(new File("C:/Users/SCY/mytruststore.jks"), "mypassword".toCharArray())
+//                .build();
 
         final ApacheHttpClient5TransportBuilder builder = ApacheHttpClient5TransportBuilder.builder(host);
         builder.setHttpClientConfigCallback(httpClientBuilder -> {
@@ -69,4 +104,6 @@ public class OpenSearchConfig {
         final OpenSearchTransport transport = builder.build();
         return new OpenSearchClient(transport);
     }
+
+    */
 }
